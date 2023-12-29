0b4f263c93962ee58b5ffef0e61d33be6f561be1
Pavel <shadow-hunter2000@yandex.ru>
2023-12-29 18:00:57 +0800

    ервый комит

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        string[] randomStrings = GenerateRandomStrings(10, 5);
+
+        Console.WriteLine("Исходные случайные строки: " + string.Join(", ", randomStrings));
+    
+    }
+}
