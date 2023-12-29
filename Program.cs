d89b1f309b64044a144dae39b183f5ee3b452efc
Pavel <shadow-hunter2000@yandex.ru>
2023-12-29 18:03:13 +0800

    торой комит

--- Program.cs
+++ Program.cs
@@ -10,4 +10,25 @@ class Program
         Console.WriteLine("Исходные случайные строки: " + string.Join(", ", randomStrings));
     
     }
+
+    static string[] GenerateRandomStrings(int count, int maxLength)
+    {
+        Random random = new Random();
+        string[] randomStrings = new string[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            int length = random.Next(1, maxLength + 1);
+            char[] chars = new char[length];
+
+            for (int j = 0; j < length; j++)
+            {
+                chars[j] = (char)random.Next('a', 'z' + 1);
+            }
+
+            randomStrings[i] = new string(chars);
+        }
+
+        return randomStrings;
+    }
 }
